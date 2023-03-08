29d6181b7ad96d1bbbb936525f427be80eded4d3
nick-epicinstitute <nadams@epicinstitute.org>
2023-03-08 11:11:15 -0800

    update filepath

--- src/pages/dash_resultsanalysis.py
+++ src/pages/dash_resultsanalysis.py
@@ -11,9 +11,13 @@ dash.register_page(
     name="Results Analysis",
 )
 
-df = pd.read_csv("/src/data/adoption_analog_output_historical.csv")
-df2 = pd.read_parquet("/src/data/adoption_output_historical.parquet").reset_index()
-df3 = pd.read_csv("/src/data/adoption_output_projections.csv")
+df = pd.read_csv(
+    "~/positive-disruption-dev/src/data/adoption_analog_output_historical.csv"
+)
+df2 = pd.read_parquet(
+    "~/positive-disruption-dev/src/data/adoption_output_historical.parquet"
+).reset_index()
+df3 = pd.read_csv("~/positive-disruption-dev/src/data/adoption_output_projections.csv")
 
 layout = html.Div(
     [
@@ -502,7 +506,7 @@ def update_graph(
 ):
     {"none": None, "tonexty": "1"}
 
-    df = pd.read_csv("/src/data/" + dataset + ".csv")
+    df = pd.read_csv("~/positive-disruption-dev/src/data/" + dataset + ".csv")
 
     fig = go.Figure()
 
@@ -580,7 +584,9 @@ def update_graph(
 
     stack_type = {"none": None, "tonexty": "1"}
 
-    df2 = pd.read_parquet("/src/data/" + dataset2 + ".parquet").reset_index()
+    df2 = pd.read_parquet(
+        "~/positive-disruption-dev/src/data/" + dataset2 + ".parquet"
+    ).reset_index()
 
     fig2 = go.Figure()
 
@@ -730,7 +736,7 @@ def update_graph(
 
     stack_type = {"none": None, "tonexty": "1"}
 
-    df3 = pd.read_csv("/src/data/" + dataset3 + ".csv")
+    df3 = pd.read_csv("~/positive-disruption-dev/src/data/" + dataset3 + ".csv")
 
     fig3 = go.Figure()
 

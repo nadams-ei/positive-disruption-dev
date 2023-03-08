1a5d8ca8e379a20150ff250f5d33bee14253ea7b
nick-epicinstitute <nadams@epicinstitute.org>
2023-03-08 11:25:55 -0800

    update filepath

--- src/pages/dash_ama.py
+++ src/pages/dash_ama.py
@@ -9,9 +9,7 @@ data_end_year = 2100
 
 dataset = ["energy_output"]
 
-df = pd.read_parquet(
-    "~/positive-disruption-dev/src/data/" + dataset[0] + ".parquet"
-).reset_index()
+df = pd.read_parquet("~/src/data/" + dataset[0] + ".parquet").reset_index()
 
 
 clst = df.columns[
